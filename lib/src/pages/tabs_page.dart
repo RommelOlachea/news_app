e58ce4d89edecb4be5c791c521200e5af9b6fb75
Rommel Olachea Cota <97263258+RommelOlachea@users.noreply.github.com>
2023-03-10 12:52:16 -0700

    implementacion del provider para cambiar de navegacion

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   const TabsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _Paginas(),
-      bottomNavigationBar: _Navegacion(),
+    return ChangeNotifierProvider(
+      create: (_) => new _NavegacionModel(),
+      child: Scaffold(
+        body: _Paginas(),
+        bottomNavigationBar: _Navegacion(),
+      ),
     );
   }
 }
@@ -20,11 +24,17 @@ class _Navegacion extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(currentIndex: 0, items: const [
-      BottomNavigationBarItem(
-          icon: Icon(Icons.person_outline), label: 'Para ti'),
-      BottomNavigationBarItem(icon: Icon(Icons.public), label: 'Encabezados'),
-    ]);
+    final navegacionMdel = Provider.of<_NavegacionModel>(context);
+
+    return BottomNavigationBar(
+        currentIndex: navegacionMdel.paginaActual,
+        onTap: (i) => navegacionMdel.paginaActual = i,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline), label: 'Para ti'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.public), label: 'Encabezados'),
+        ]);
   }
 }
 
@@ -46,5 +56,16 @@ class _Paginas extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class _NavegacionModel extends ChangeNotifier {
+  int _paginaActual = 0;
+
+  int get paginaActual => this._paginaActual;
+
+  set paginaActual(int valor) {
+    this._paginaActual = valor;
+    notifyListeners();
   }
 }
