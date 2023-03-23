7278fa6b03291b493efbe1de473b7034bdbb9e32
RommelOlachea <msc.rommel.olachea@gmail.com>
2023-03-22 20:16:11 -0700

    se mantiene el estado del scroll de la primera pantalla

--- lib/src/pages/tab1_page.dart
+++ lib/src/pages/tab1_page.dart
@@ -3,9 +3,15 @@ import 'package:newsapp/src/services/news_service.dart';
 import 'package:newsapp/src/widgets/lista_noticias.dart';
 import 'package:provider/provider.dart';
 
-class Tab1Page extends StatelessWidget {
+class Tab1Page extends StatefulWidget {
   const Tab1Page({Key? key}) : super(key: key);
 
+  @override
+  State<Tab1Page> createState() => _Tab1PageState();
+}
+
+class _Tab1PageState extends State<Tab1Page>
+    with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
     final headlines = Provider.of<NewsService>(context).headlines;
@@ -18,4 +24,8 @@ class Tab1Page extends StatelessWidget {
           : ListaNoticias(headlines),
     );
   }
+
+  @override
+  bool get wantKeepAlive =>
+      true; //con esto no destruimos la pantalla y se mantiene el estado
 }
